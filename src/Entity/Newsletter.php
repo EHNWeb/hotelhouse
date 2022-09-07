7de06803159813653144da1ba603c40082378635
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-09-07 23:32:55 +0200

    Add Newsletter Entity
    Signed-off-by: HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>

---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsletterRepository::class)
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $souscription;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_enregistrement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function isSouscription(): ?bool
+    {
+        return $this->souscription;
+    }
+
+    public function setSouscription(bool $souscription): self
+    {
+        $this->souscription = $souscription;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $date_enregistrement): self
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+}
