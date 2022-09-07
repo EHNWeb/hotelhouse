72fbe9c838e5a11c75ecebcb71f7ca02f466352c
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-09-07 17:04:31 +0200

    Add Membre CRUD
    Signed-off-by: HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>

--- src/Controller/Admin/MembreCrudController.php
+++ src/Controller/Admin/MembreCrudController.php
@@ -3,23 +3,71 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Membre;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class MembreCrudController extends AbstractCrudController
 {
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+    
     public static function getEntityFqcn(): string
     {
         return Membre::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('pseudo', 'Pseudo'),
+            TextField::new('password', 'Mot de passe')
+                ->setFormType(PasswordType::class)
+                ->onlyOnForms()
+                ->onlyWhenCreating(),
+            EmailField::new('email', 'E-mail'),
+            ChoiceField::new('civilite', 'Civilité')->setChoices([
+                'Monsieur' => 'm',
+                'Madame' => 'f',
+                'Mx' => 'x',
+            ]),
+            TextField::new('nom', 'Nom'),
+            TextField::new('prenom', 'Prénom'),
+            TelephoneField::new('telephone', 'Téléphone'),
+            CollectionField::new('roles', 'Roles')->setTemplatePath('admin/field/roles.html.twig'),
+            DateTimeField::new('DateEnregistrement', "Date<br>Enregistrement")->setFormat("dd/MM/Y HH:mm:ss")->onlyOnIndex(),
         ];
     }
-    */
+
+    public function createEntity(string $entityFqcn)
+    {
+        $Membre = new Membre();
+        $Membre->setDateEnregistrement(new \DateTime);
+
+        return $Membre;
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance->getId()) {
+            $entityInstance->setPassword(
+                $this->hasher->hashPassword($entityInstance, $entityInstance->getPassword())
+            );
+        }
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
 }
